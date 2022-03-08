82283c396c1f31b38cc5acbc29cc636f40118dad
kasiabzemka <101147074+kasiabzemka@users.noreply.github.com>
2022-03-08 07:13:24 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function submit(question) {
+    const answer = document.getElementById("answer").value;
+    fetch(`https://9gu8mm7khg.execute-api.us-east-1.amazonaws.com/test/awesomebirthdayapi?answer=${answer}&question=${question}`, {headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+        }})
+        .then(response => { return response.json()})
+        .then(response => {
+            if (response.status === 200) {
+                document.getElementById("instructions").style.display = "none";
+                const correct = document.getElementById("correct");
+                correct.style.display = "block";
+                correct.innerHTML = response.message;
+            } else if (response.status === 400) {
+                const wrong = document.getElementById("wrong").style.display = "block";
+                correct.style.display = "block";
+            }
+        })
+};
